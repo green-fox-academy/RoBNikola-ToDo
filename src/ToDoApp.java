3125dfc6cc071f6fff2265d438a5d1fb65a72d57
RobNikola911 <pajic1991@gmail.com>
2020-09-24 18:22:35 +0200

    ToDoApp

--- src/ToDoApp.java
+++ src/ToDoApp.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ToDoApp {
-    static Path filePath = Paths.get("C:\\greenfoxtodoproject\\RoBNikola-ToDo\\src\\text.txt");
+    static Path filePath = Paths.get("src/text.txt");
     static List<String> lines = new ArrayList<String>();
 
     //  Command line arguments:
@@ -20,7 +20,7 @@ public class ToDoApp {
                 System.out.println("Unable to add: no task provided");
             } else if (args[0].equals("-a") && args.length == 2) {
                 System.out.println(args[1]);
-                String theArg = "\n" + args[1];
+                String theArg = "\n" + "[ ] " + args[1];
                 Files.write(filePath, theArg.getBytes(), StandardOpenOption.APPEND);
             } else if (args[0].equals("-a") && args.length >= 2) {
                 System.out.println("Unable to add: more than 1 task provided - try in/between qoute marks");
@@ -37,13 +37,26 @@ public class ToDoApp {
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("There is no " + args[1] + ". line. Unable to remove: index is out of bound");
                 }
-            } else {
+            } else if (args[0].equals("-r")) {
                 System.out.println("Unable to remove: no index provided");
-//                listMyTasks(String[] args);
+            } else if (args[0].equals("-c") && lines.size() >= 2 && args.length == 2) {
+                int i = Integer.parseInt(args[1]);
+                checkTask(i - 1);
+            } else {
+                System.out.println("Unsupported argument");
+                usageInformation();
             }
         } catch (NoSuchFileException e) {
             System.out.println("Uh-oh, could not read the file!");
+        } catch (IndexOutOfBoundsException e) {
+            usageInformation();
         }
+    }
+
+    public static void checkTask(int i) throws IOException {
+//        lines.set(i, "[x] " + lines);
+//        lines.add(i, "[x] ");
+        Files.write(filePath, lines);
     }
 
     public static void removeThisLine(int i) throws IOException {
@@ -57,5 +70,17 @@ public class ToDoApp {
             System.out.print(++index + ". ");
             System.out.println(tasks);
         }
+    }
+
+    public static void usageInformation() {
+        System.out.println();
+        System.out.println("Command Line Todo application");
+        System.out.println("=============================");
+        System.out.println();
+        System.out.println("Command line arguments:");
+        System.out.println("    -l   Lists all the tasks");
+        System.out.println("    -a   Adds a new task");
+        System.out.println("    -r   Removes a task");
+        System.out.println("    -c   Completes a task");
     }
 }
